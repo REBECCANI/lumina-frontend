1cf5c678aa9efd4453d369ff8e45d5f18920f33e
REBECCANI <beckynishimwe@example.com>
2024-09-09 11:31:32 +0200

    upd

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -76,7 +76,7 @@ const RegisterForm = () => {
     return (
         <div className="h-full my-12">
             <div className="flex justify-center items-center min-h-screen">
-                <div className="flex flex-col bg-white w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2 2xl:w-1/3 bg-opacity-50 rounded-3xl shadow-xl p-4 md:p-8 lg:p-10">
+                <div className="flex flex-col bg-white w-full sm:w-4/5 md:w-3/4 lg:w-3/4 xl:w-2/3 2xl:w-1/2 bg-opacity-50 rounded-3xl shadow-xl p-4 md:p-8 lg:p-10">
                     <div>
                         <div className="flex flex-col items-center justify-center">
                             <h1 className="text-primary font-poppins mb-4 mt-4 font-bold text-black text-2xl md:text-3xl lg:text-4xl xl:text-5xl">CONTACT US</h1>
