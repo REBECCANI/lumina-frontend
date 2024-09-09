114e69c740c0e6d993d2631020f89a4835f5ddf0
REBECCANI <beckynishimwe@example.com>
2024-09-09 14:29:16 +0200

    u

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -282,7 +282,7 @@ const BookingPage = () => {
                                 <label htmlFor="firstName" className="text-black font-bold mb-1">First Name</label>
                                 <input
                                     id="firstName"
-                                    className="bg-yellow bg-opacity-20 rounded-full px-2 py-1"
+                                    className="bg-gray-200 rounded-full px-4 py-2"
                                     type="text"
                                     onChange={(e) => setFirstName(e.target.value)}
                                     onClick={resetError}
