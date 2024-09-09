d6b784ddde275ca089d9f63e4a5f521d9c7e958a
REBECCANI <beckynishimwe@example.com>
2024-09-09 17:21:50 +0200

    u

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,15 +1,18 @@
 // src/components/Navbar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
-
 const Navbar: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div>
-            {/* Main Container for Logo and Navigation Links */}
             <div className="bg-yellow-800 p-4 fixed w-full top-0 left-0 z-8">
                 <div className="container mx-auto flex justify-between items-center h-6">
-                    {/* Logo */}
                     <Link href="/">
                         <img
                             src="/logo.jpeg"
@@ -17,8 +20,6 @@ const Navbar: React.FC = () => {
                             className="h-12 w-14 cursor-pointer"
                         />
                     </Link>
-
-                    {/* Navigation Links */}
                     <div className="hidden md:flex flex-grow justify-center space-x-4">
                         <Link href="/">
                             <span className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Home</span>
@@ -33,17 +34,13 @@ const Navbar: React.FC = () => {
                             <span className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Contact</span>
                         </Link>
                     </div>
-
-                    {/* Mobile Menu Button */}
-                    <button className="md:hidden text-white focus:outline-none ml-4" id="menu-button">
+                    <button onClick={toggleMenu} className="md:hidden text-white focus:outline-none ml-4">
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                         </svg>
                     </button>
                 </div>
-
-                {/* Mobile Menu */}
-                <div className="md:hidden hidden" id="menu">
+                <div className={`${isMenuOpen ? '' : 'hidden'} md:hidden`}>
                     <Link href="/">
                         <span className="block text-white hover:bg-gray-700 px-3 py-2 rounded-md text-base font-medium">Home</span>
                     </Link>
